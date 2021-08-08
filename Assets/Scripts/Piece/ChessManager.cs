60ba71338006ecf74038bd59f4a9b7e995c714b7
suan9340 <sue9340@gmail.com>
2021-08-08 14:32:38 +0900

    d

--- Assets/Scripts/Piece/ChessManager.cs
+++ Assets/Scripts/Piece/ChessManager.cs
@@ -11,7 +11,7 @@ public class ChessManager : MonoBehaviour
     public bool isAttacking = false;
     void Start()
     {
-        Instantiate(White_Bishop,new Vector3(-2.5f,-1.8f,0f),Quaternion.identity);
+        ChessSetting();
     }
 
     // Update is called once per frame
@@ -26,5 +26,19 @@ public class ChessManager : MonoBehaviour
     public void SetAttack(bool _isAttack)
     {
         isAttacking = _isAttack;
+    }
+
+    private void ChessSetting()
+    {
+
+        SetPosition(White_Pawn, -2.4f, -1.8f); SetPosition(White_Pawn, -1.72f, -1.8f); SetPosition(White_Pawn, -1.03f, -1.8f); SetPosition(White_Pawn, -0.336f, -1.8f);
+        SetPosition(White_Pawn, 0.343f, -1.8f); SetPosition(White_Pawn, 1.05f, -1.8f); SetPosition(White_Pawn, 1.73f, -1.8f); SetPosition(White_Pawn, 2.41f, -1.8f);
+        SetPosition(White_Rook, -2.4f, -2.53f); SetPosition(White_Knight, -1.72f, -2.53f); SetPosition(White_Knight, -1.03f, -2.53f); SetPosition(White_Bishop, -0.336f, -2.53f);
+        SetPosition(White_Queen, 0.343f, -2.53f); SetPosition(White_King, 1.05f, -2.53f); SetPosition(White_Bishop, 1.73f, -2.53f); SetPosition(White_Rook, 2.41f, -2.53f);
+    }
+
+    private void SetPosition(GameObject _chessName, float _x, float _y)
+    {
+        Instantiate(_chessName, new Vector3(_x, _y, 0f), Quaternion.identity);
     }
 }
