847411357ce7d6338b638a557e667b5667451d4e
minyoung529 <77655318+minyoung529@users.noreply.github.com>
2021-08-19 00:40:58 +0900

    앙파상 구현중 (아직 다른 곳으로 이동하는 거 XX)

--- Assets/Scripts/Piece/Pawn.cs
+++ Assets/Scripts/Piece/Pawn.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Pawn : ChessBase
 {
+    private int originPosX;
+    private bool isEnpassant = false;
+
+    private void Start()
+    {
+        originPosX = xBoard;
+    }
+
     public override void MovePlate()
     {
         P_MovePlate();
@@ -11,7 +19,7 @@ public class Pawn : ChessBase
 
     private void P_MovePlate()
     {
-        if(player == "white")
+        if (player == "white")
         {
             PawnMovePlate(xBoard, yBoard + 1);
         }
@@ -32,7 +40,6 @@ public class Pawn : ChessBase
 
                 else
                 {
-                   
                     if (player == "white")
                     {
                         GameManager.Inst.MovePlateSpawn(x, y, this);
@@ -59,6 +66,28 @@ public class Pawn : ChessBase
                 ChessManager.Inst.GetPosition(x - 1, y).GetComponent<ChessBase>().player != player)
             {
                 GameManager.Inst.MovePlateAttackSpawn(x - 1, y, this);
+            }
+        }
+
+        if (!isEnpassant)
+        {
+            if (moveCnt == 2 && xBoard == originPosX && yBoard == 4)
+            {
+                isEnpassant = true;
+                if (player == "white") y -= 1;
+                else if (player == "black") y += 1;
+
+                if (ChessManager.Inst.PositionOnBoard(x + 1, y) && ChessManager.Inst.GetPosition(x + 1, y) != null &&
+               ChessManager.Inst.GetPosition(x + 1, y).GetComponent<ChessBase>().player != player)
+                {
+                    GameManager.Inst.MovePlateAttackSpawn(x + 1, y, this);
+                }
+
+                if (ChessManager.Inst.PositionOnBoard(x - 1, y) && ChessManager.Inst.GetPosition(x - 1, y) != null &&
+               ChessManager.Inst.GetPosition(x - 1, y).GetComponent<ChessBase>().player != player)
+                {
+                    GameManager.Inst.MovePlateAttackSpawn(x + 1, y, this);
+                }
             }
         }
     }
